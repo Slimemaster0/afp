e0a6e52cafd961f5ecf450248fc9db7bb9938e06
Slimemaster <milo@munksgaard.eu>
2022-12-12 18:14:34 +0100

    Move away from org

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,45 @@
+// std
 pub use std::process::Command; // Executing commands
 use std::env; // Reading the environment
 
+// Colord
 use colored::Colorize; // Colors
 
+// Serde
 pub use serde::{Deserialize, Serialize};
 
-struct Exec {
-    cmd: String,
-    args: Vec<String>
+
+struct Exec { // A struct for running commands
+    cmd: String, // The command
+    args: Vec<String> // The arguments
 }
 
 impl Exec {
+    fn get_output(&self) -> String { // Runs the command and returns the output.
+        let mut cmd = Command::new(&self.cmd); // Converts the cmd string to a 'Command'
 
-fn get_output(&self) -> String {
-    let exec = &self.cmd;
-    let mut cmd = Command::new(exec);
+        // adding the arguments
+        for arg in self.args.iter() {
+            cmd.arg(arg);
+        }
 
-    // adding the arguments
-    for arg in self.args.iter() {
-        cmd.arg(arg);
-    }
-
-    match cmd.output() {
-        Ok(o) =>  {
-            unsafe {
-                // Make a string from the output of the command.
-                let mut str = String::from_utf8_unchecked(o.stdout);
-                // Remove the new line charactor and return the output
-                return trim_newline(&mut str);
+        match cmd.output() {
+            Ok(o) =>  {
+                unsafe {
+                    let mut str = String::from_utf8_unchecked(o.stdout); // Make a string from the output of the command.
+                    return trim_newline(&mut str); // Remove the new line charactor and return the output
+                }
+            },
+            Err(e) => {
+                println!("Err: {}", e);
+                return "N/A".to_string();
             }
-        },
-        Err(e) => {
-            println!("Err: {}", e);
-            return "N/A".to_string();
         }
     }
+    // End of impl Exec
 }
 
-}
+
 
 #[derive(Default, Clone, Deserialize)]
 struct OsinfoOpt {
@@ -62,32 +64,25 @@ struct Osinfo {
     KernelRelease: String,
 }
 
-fn main() {
+fn main() { // main function
+    let osinfo = get_osinfo(); // get the OS information
+    let user_name = Exec { cmd: "whoami".to_string(), args: vec![] }; // prepare the whoami command 
 
-let osinfo = get_osinfo();
+    println!("{} {}", format!("Distro:").blue().bold(), osinfo.OSPretty ); // Prints the distro name
+    println!("{}@{}", format!("{}", osinfo.Hostname).blue().bold(), format!("{}", user_name.get_output()).green() ); // Prints the user name and the hostname
+    println!("{} {} {}", format!("Kernel:").blue().bold(), osinfo.KernelName, format!("{}", osinfo.KernelRelease).green() ); // Prints the kernel name and version
+    println!("{} {}", format!("Device:").blue().bold(), osinfo.HardwareModel ); // Prints the hardware model
+    println!("{} {}", format!("Vendor:").blue().bold(), osinfo.HardwareVendor ); // Prints the hardware vendor
 
-println!("{} {}", format!("Distro:").blue().bold(), osinfo.OSPretty );
-
-
-
-let user_name = Exec { cmd: "whoami".to_string(), args: vec![] };
-
-println!("{}@{}", format!("{}", osinfo.Hostname).blue().bold(), format!("{}", user_name.get_output()).green() );
-
-println!("{} {} {}", format!("Kernel:").blue().bold(), osinfo.KernelName, format!("{}", osinfo.KernelRelease).green() ); // Print output
-
-println!("{} {}", format!("Device:").blue().bold(), osinfo.HardwareModel );
-
-println!("{} {}", format!("Vendor:").blue().bold(), osinfo.HardwareVendor );
-
-match env::var("EDITOR") {
-    Ok(v) => println!("{} {}", format!("Editor:").blue().bold(), v),
-    Err(_e) => nop()
-};
-
+    match env::var("EDITOR") { // Looks for the editor EnvVar
+        Ok(v) => println!("{} {}", format!("Editor:").blue().bold(), v), // Prints the EDITOR variable if it exits
+        Err(_e) => nop() // Does nothing
+    };
 }
 
-fn trim_newline(s: &mut String) -> String {
+// Common functions
+
+fn trim_newline(s: &mut String) -> String { // Trims new line chars
     if s.ends_with('\n') {
         s.pop();
         if s.ends_with('\r') {
@@ -97,15 +92,17 @@ fn trim_newline(s: &mut String) -> String {
     return s.to_string();
 }
 
-fn unopt_String(opt: Option<String>) -> String {
+// Convert Option<T> to normal types
+
+fn unopt_String(opt: Option<String>) -> String { // Converts 'Option<String>' to 'String'
     match opt {
-        Some(s) => return s,
-        None => return "".to_string()
+        Some(s) => return s, // Return the String
+        None => return "".to_string() // Return an empty string
     }
 }
 
 impl OsinfoOpt {
-    fn to_norm(self) -> Osinfo {
+    fn to_norm(self) -> Osinfo { // Converts 'OsinfoOpt' to osinfo
         let opt = self;
         let hwv: String = unopt_String(opt.HardwareVendor);
         let hwm: String = unopt_String(opt.HardwareModel);
@@ -121,15 +118,15 @@ impl OsinfoOpt {
     }
 }
 
-fn get_osinfo() -> Osinfo {
-    let info_command = Exec { cmd: "hostnamectl".to_string(), args: vec!["--json=short".to_string()] };
+fn get_osinfo() -> Osinfo { // Returns the OS information as 'Osinfo'
+    let info_command = Exec { cmd: "hostnamectl".to_string(), args: vec!["--json=short".to_string()] }; // Returns the OS information as json 'String'
 
-    let info_json_string = info_command.get_output();
-    let info_json_str: &str = &info_json_string;
+    let info_json_string = info_command.get_output(); // Runs 'info_command'
+    let info_json_str: &str = &info_json_string; // Converts info_json_string to &str
 
-    let osinfo: OsinfoOpt = serde_json::from_str(&info_json_str).expect("Err: could not parse json.");
+    let osinfo: OsinfoOpt = serde_json::from_str(&info_json_str).expect("Err: could not parse json."); // Parses json 
 
-    return osinfo.to_norm();
+    return osinfo.to_norm(); // Converts osinfo from OsinfoOpt to Osinfo and returns it
 }
 
-fn nop() {}
+fn nop() {} // A function does nothing™
